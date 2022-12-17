f430cfc58312f7a9df24353bc2f14096b65259d4
Finn Chen <finn.chen.maenhaut@gmail.com>
2022-12-16 19:56:56 -0500

    commit tree d3f122b3d92afb2e8c39c3468cecaf6c9fe788f1

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
+//const { update } = require("jsdom/lib/jsdom/living/html-collection");
+
 // Write your solution in this file!
+const employee = {
+  name: "Finn",
+  streetAddress: "NewYork",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  console.log(employee);
+  console.log(key);
+  console.log(value);
+  const newEmployee = { ...employee };
+  newEmployee[key] = value;
+  return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+function deleteFromEmployeeByKey(employee, key, value) {
+  const deletedEmployee = { ...employee };
+  delete deletedEmployee.name;
+  return deletedEmployee;
+  console.log(deletedEmployee);
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+  delete employee.name;
+  return employee;
+  console.log(employee);
+}
+
+// const updateEmployee = updateEmployeeWithKeyAndValue(
+//   employee,
+//   "streetAddress",
+//   "Montreal"
+// );
+// const updateEmployee2 = updateEmployeeWithKeyAndValue(
+//   updateEmployee,
+//   "name",
+//   "Joe"
+// );
+// console.log(updateEmployee);
+// console.log(updateEmployee2);
